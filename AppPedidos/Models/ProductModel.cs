f74aed3306ac49984d53aa794d62252643e85617
Rodri080604 <huarachirodrigo76@gmail.com>
2025-09-16 21:08:52 -0400

    Creación y validación de ProductModel

---
+++ AppPedidos/Models/ProductModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppPedidos.Models
+{
+    public class ProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre no puede exceder 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [StringLength(500, ErrorMessage = "La descripción no puede exceder 500 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+        public decimal Precio { get; set; }
+
+        [Required(ErrorMessage = "El stock es obligatorio")]
+        [Range(0, int.MaxValue, ErrorMessage = "El stock no puede ser negativo")]
+        public int Stock { get; set; }
+    }
+}
