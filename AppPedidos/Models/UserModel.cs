66a5ae062e71ee8aee962a219f1cdc8e3878dab1
Rodri080604 <huarachirodrigo76@gmail.com>
2025-09-16 21:08:03 -0400

    Creación y validación de UserModel

---
+++ AppPedidos/Models/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppPedidos.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre no puede exceder 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El email es obligatorio")]
+        [EmailAddress(ErrorMessage = "El formato del email no es válido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "La contraseña debe tener entre 6 y 100 caracteres")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "El rol es obligatorio")]
+        public Role Rol { get; set; }
+        public enum Role
+        {
+            Admin,
+            Cliente,
+            Empleado
+        }
+        
+    }
+}
