6e6153f44431555754f97cd9472c9c5ae3b52781
Rodri080604 <huarachirodrigo76@gmail.com>
2025-09-16 21:09:32 -0400

    Creación y validación de OrderModel

---
+++ AppPedidos/Models/OrderModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppPedidos.Models
+{
+    public class OrderModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El cliente es obligatorio")]
+        public int ClienteId { get; set; }
+
+        [ForeignKey("ClienteId")]
+        public UserModel Cliente { get; set; }
+
+        [Required(ErrorMessage = "La fecha es obligatoria")]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "El estado es obligatorio")]
+        public OrderStatus Estado { get; set; }
+
+        [Required(ErrorMessage = "El total es obligatorio")]
+        [Range(0, double.MaxValue, ErrorMessage = "El total no puede ser negativo")]
+        public decimal Total { get; set; }
+
+        public List<OrderItemModel> Items { get; set; } = new List<OrderItemModel>();
+    }
+    public enum OrderStatus
+    {
+        Pendiente,
+        Procesado,
+        Enviado,
+        Entregado
+    }
+}
